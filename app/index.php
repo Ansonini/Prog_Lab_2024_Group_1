b21e70e626e427924bd3e333439d89bad9f56354
Jean-Gabriel Hanania <83063812+jghanania@users.noreply.github.com>
2024-05-31 17:22:08 +0200

    some comments + more queries

--- app/index.php
+++ app/index.php
@@ -14,14 +14,28 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT orderItemID, sku , orderID FROM orderItems where orderID=100";
-$result = $conn->query($sql);
-
+// Open some .html files
 $htmlcontent = file_get_contents('Frontend/managment_graph.html');
 echo $htmlcontent;
 
 $htmlcontent2 = file_get_contents('Frontend/storeInfo.html');
 echo $htmlcontent2;
+
+// Make some sql queries
+$sql = "SELECT orderItemID, sku , orderID FROM orderItems where orderID=100";
+$result = $conn->query($sql);
+
+$sql2 = "SELECT p.pizzaName , p.pizzaSize , p.Price , COUNT(oi.SKU) as soldPizzas, p.Price * count(oi.SKU) as 'Revenue/pizza'
+        FROM products p 
+        JOIN orderItems oi on oi.SKU=p.SKU 
+        JOIN orders o on o.orderID = oi.orderID 
+        GROUP BY p.pizzaName, p.pizzaSize , p.Price
+        ;";
+$result2 = $conn->query($sql2);
+
+// Display result from sql  
+echo "<br>
+    Items in Order number 100";
 
 if ($result->num_rows > 0) {
     // Output data of each row
@@ -36,6 +50,34 @@ if ($result->num_rows > 0) {
                 <td>" . $row["orderItemID"]. "</td>
                 <td>" . $row["sku"]. "</td>
                 <td>" . $row["orderID"]. "</td>
+              </tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+
+echo "<br>
+    Revenue per pizza type and size";
+
+if ($result2->num_rows > 0) {
+    // Output data of each row
+    echo "<table border='1'>
+            <tr>
+                <th>pizzaName</th>
+                <th>pizzaSize</th>
+                <th>Price</th>
+                <th>soldPizzas</th>
+                <th>Revenue/pizza</th>
+            </tr>";
+    while($row = $result2->fetch_assoc()) {
+        echo "<tr>
+                <td>" . $row["pizzaName"]. "</td>
+                <td>" . $row["pizzaSize"]. "</td>
+                <td>" . $row["Price"]. "</td>
+                <td>" . $row["soldPizzas"]. "</td>
+                <td>" . $row["Revenue/pizza"]. "</td>
               </tr>";
     }
     echo "</table>";

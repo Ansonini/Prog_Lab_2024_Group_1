b18ea67e801eae9c30fd878b05abfa061c5cf90b
Jean-Gabriel Hanania <83063812+jghanania@users.noreply.github.com>
2024-05-31 12:38:06 +0200

    .csv datein und fertiger docker compose
    orders.csv in zwei geschnitten da github nur datein bis 100mb erlaubt

--- Backend/app/index.php
+++ Backend/app/index.php
@@ -2,7 +2,7 @@
 $servername = "mysql"; // This should match the service name in your docker-compose.yml
 $username = "root";
 $password = "1234";
-$dbname = "pizza";
+$dbname = "pizzaDB";
 $port = 3306;
 
 
@@ -14,7 +14,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT sku, pizzaName, price FROM products";
+$sql = "SELECT orderItemID, sku , orderID FROM orderItems where orderID=100";
 $result = $conn->query($sql);
 
 
@@ -22,15 +22,15 @@ if ($result->num_rows > 0) {
     // Output data of each row
     echo "<table border='1'>
             <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Price</th>
+                <th>orderItemID</th>
+                <th>sku</th>
+                <th>orderID</th>
             </tr>";
     while($row = $result->fetch_assoc()) {
         echo "<tr>
+                <td>" . $row["orderItemID"]. "</td>
                 <td>" . $row["sku"]. "</td>
-                <td>" . $row["pizzaName"]. "</td>
-                <td>" . $row["price"]. "</td>
+                <td>" . $row["orderID"]. "</td>
               </tr>";
     }
     echo "</table>";

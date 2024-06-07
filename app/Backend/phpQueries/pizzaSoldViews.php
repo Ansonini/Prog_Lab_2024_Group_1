913fba2b2992efafd8990502dcbbefd7fb227786
Jean-Gabriel Hanania <83063812+jghanania@users.noreply.github.com>
2024-06-07 14:39:06 +0200

    added per store/ not ready

--- app/Backend/phpQueries/pizzaSoldViews.php
+++ app/Backend/phpQueries/pizzaSoldViews.php
@@ -7,10 +7,10 @@ global $conn;
 function getSalesFromWeek($year, $week, $mode)
 {
   global $conn;
-if($mode == 'units') {
+  if ($mode == 'units') {
 
     $sql =
-    "SELECT   YEAR(o.orderDate)     as sellingYear,
+      "SELECT   YEAR(o.orderDate)     as sellingYear,
               week(o.orderDate, 1)  as sellingWeek,
               DAYNAME(o.orderDate)  as sellingDay,
               sum(o.nItems)         as pizzaSold
@@ -19,11 +19,9 @@ if($mode == 'units') {
     AND WEEK(o.orderDate, 1) = $week
     GROUP BY sellingYear, sellingWeek, weekday(o.orderDate), sellingDay
     ORDER BY sellingYear, sellingweek, weekday(o.orderDate)";
+  } elseif ($mode == 'revenue') {
 
-}
-elseif($mode == 'revenue') {
-
-  $sql = "SELECT  YEAR(o.orderDate)     as sellingYear,
+    $sql = "SELECT  YEAR(o.orderDate)     as sellingYear,
                   week(o.orderDate, 1)  as sellingWeek,
                   DAYNAME(o.orderDate)  as sellingDay,
                   sum(o.total)          as revenuePerDay
@@ -32,12 +30,9 @@ elseif($mode == 'revenue') {
           AND WEEK(o.orderDate, 1) = $week
         GROUP BY sellingYear, sellingWeek, weekday(o.orderDate), sellingDay
         ORDER BY sellingYear, sellingweek, weekday(o.orderDate)";
-
-}
-else
-{
-  echo 'no valid mode selected, chose units or revenue';
-}
+  } else {
+    echo 'no valid mode selected, chose units or revenue';
+  }
 
 
 
@@ -55,4 +50,53 @@ else
 
   return json_encode($data);
 }
-?>
+
+
+
+
+function getSalesFromWeekPerStore($year, $week, $mode)
+{
+  if ($mode == 'units') {
+
+    global $conn;
+    $sql =
+
+      "SELECT  o.storeID,
+      sum(o.nItems)   as unitsPerDay
+      FROM orders o
+      WHERE YEAR(o.orderDate) = $year
+      AND WEEK(o.orderDate, 1) = $week
+      GROUP BY o.storeID
+      ORDER BY unitsPerDay desc";
+
+  } elseif ($mode == 'revenue') {
+
+    $sql = "SELECT  o.storeID,
+    sum(o.total)   as revenuePerDay
+    FROM orders o
+    WHERE YEAR(o.orderDate) = $year
+    AND WEEK(o.orderDate, 1) = $week
+    GROUP BY o.storeID
+    ORDER BY unitsPerDay desc";
+
+  } else {
+    echo 'no valid mode selected, chose units or revenue';
+  }
+
+  $result = $conn->query($sql);
+
+  $data = array();
+  if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+      $data[] = $row;
+    }
+  } else {
+    $data[] = "0 results";
+  }
+  $conn->close();
+
+  return json_encode($data);
+
+
+
+}

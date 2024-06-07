5670999bd60be47747134535ddae199b35760301
Jean-Gabriel Hanania <83063812+jghanania@users.noreply.github.com>
2024-06-07 15:22:19 +0200

    took out the html code into another file. Index.php now is used to include code from other files --> less conflict when changing code

--- app/index.php
+++ app/index.php
@@ -4,11 +4,10 @@ include 'Backend/phpQueries/pizzaSoldViews.php';
 include 'Backend/connectDB.php';
 
 // Open some .html files
-$htmlcontent = file_get_contents('Frontend/managment_graph.html');
-echo $htmlcontent;
+include 'Frontend/managment_graph.html';
 
-$htmlcontent2 = file_get_contents('Frontend/storeInfo.html');
-echo $htmlcontent2;
+
+include 'Frontend/storeInfo.html';
 
 
 // Choose here what .php file to test. It needs to return json data 
@@ -16,66 +15,12 @@ $pathToPhpFile = "Backend/phpQueries/pizzaHeatMap.php";
 include 'Backend/phpQueries/testPhpFile.html';
 
 
-$result = getSalesFromWeek(2020,1,'units');
+$result = getSalesFromWeek(2020,2,'units');
 
 // Save the result in a variable
 $data = json_decode($result, true);
 
-// Do something with $data
+include 'Backend/phpQueries/displayJsonTable.html'
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Pizza Sales Data</title>
-    <style>
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        th, td {
-            border: 1px solid black;
-            padding: 8px;
-            text-align: left;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
-</head>
-<body>
 
-<h1>Pizza Sales Data</h1>
-
-<table>
-    <tr>
-        <?php
-        // Generate table headers dynamically based on the first row of data
-        if (!empty($data) && is_array($data)) {
-            $firstRow = $data[0];
-            foreach ($firstRow as $key => $value) {
-                echo "<th>" . htmlspecialchars($key) . "</th>";
-            }
-        } else {
-            echo "<th>No data available</th>";
-        }
-        ?>
-    </tr>
-    <?php
-    if (!empty($data) && is_array($data)) {
-        foreach ($data as $row) {
-            echo "<tr>";
-            foreach ($row as $value) {
-                echo "<td>" . htmlspecialchars($value) . "</td>";
-            }
-            echo "</tr>";
-        }
-    } else {
-        echo "<tr><td colspan='" . (count($firstRow) ?? 1) . "'>No results found</td></tr>";
-    }
-    ?>
-</table>
-
-</body>
-</html>

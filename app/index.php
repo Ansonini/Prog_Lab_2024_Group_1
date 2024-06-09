cb042786fb64d5148f486fb37a466ac081eaf421
Jean-Gabriel Hanania <83063812+jghanania@users.noreply.github.com>
2024-06-09 13:46:42 +0200

    modified js to only show and wait for relevant input. Remove old code from index

--- app/index.php
+++ app/index.php
@@ -8,18 +8,5 @@ include 'conf/connectDB.php';
 // include 'Frontend/storeInfo.html';
 
 include 'ajaxExample.html';
-
-// To test a single function .php file and display the table it returns. Choose here what .php file to test. It needs to return json data 
-// $pathToPhpFile = "Backend/phpQueries/pizzaHeatMap.php"; 
-// include 'Backend/phpQueries/testPhpFile.html';              // .html file to show a button that only activate the previous .php file upon clicking it and display the resulting table
-
-
-
-// // Add any .php file of which you want to use a function of directly in index.php for testing purpose. Will be deleted at end 
-// include 'Backend/phpQueries/pizzaSoldViews.php';
-
-// $result = getSalesFromWeek(2020,2,'units');         // Select the function you want to test
-//include 'Backend/phpQueries/displayJsonTable.html'          // .html file to display any json result as Table with this file
-
 ?>
 

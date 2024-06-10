1130ca281a296556c3ee6b75b6aba5771d90bfe1
Sargis <lucian-zimmermann@protonmail.com>
2024-06-10 20:42:35 +0200

    Working on the implementation of the backend data. However added the layout of how the site should look

--- app/Frontend/javascript/over-view.js
+++ app/Frontend/javascript/over-view.js
@@ -22,149 +22,11 @@ function lineChartSales() {
     };
 
     overallSold.setOption(option);
+    option && overallSold.setOption(option);
+    overallSold.resize({width: 1000, height: 500})
 
-    window.addEventListener('resize', function () {
-        overallSold.resize({width: 1000, height: 500});
-    });
 }
-/*  // trying to fetch data from the database to display in the echarts line chart
-    const overallSold = echarts.init(document.getElementById('line-chart-sales'));
-    fetch('app/Backend/phpQueries/pizzaBoughtTogethers.php')
-        .then(response => response.json())
-        .then(data => {
-            var option = {
-                title: {
-                    text: 'Number of Pizzas Sold',
-                    left: 'center'
-                },
-                xAxis: {
-                    type: 'category',
-                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-                },
-                yAxis: {
-                    type: 'value',
-                    min: 0
-                },
-                series: [
-                    {
-                        data: data,
-                        type: 'line'
-                    }
-                ]
-            };
-            overallSold.setOption(option);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-}
-
-
-             old test code using the Chart.js framework instead of echarts
-           var ctx = document.getElementById('line-chart-sales').getContext('2d');
-           new Chart(ctx, {
-               type: 'bar',
-               data: {
-                   labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
-                   datasets: [{
-                       label: 'Number of Pizzas Sold',
-                       data: [12, 19, 3, 5, 2, 3, 9],
-                       backgroundColor: [
-                           'rgba(54, 162, 235, 0.2)',
-                       ],
-                       borderColor: [
-                           'rgba(54, 162, 235, 1)',
-                       ],
-                       borderWidth: 1
-                   }]
-               },
-               options: {
-                   responsive: true,
-                   maintainAspectRatio: true,
-                   aspectRatio: 2.5,
-                   scales: {
-                       yAxes: [{
-                           ticks: {
-                               beginAtZero: true
-                           }
-                       }]
-                   }
-               }
-           });
-
-       }*/
-
 function storesBarChart(){
-    var sortedBarchartStores = echarts.init(document.getElementById('stores-sold'));
-    var option = {
-        dataset: [
-            {
-                dimensions: ['storeID','score'],
-                source: [
-                    ['Store-1',314],
-                    ['Store-2',351],
-                    ['Store-3',287],
-                    ['Store-4',219],
-                    ['Store-5',253],
-                    ['Store-6',165],
-                    ['Store-7',318],
-                    ['Store-8',366]
-                ]
-            },
-            {
-                transform: {
-                    type: 'sort',
-                    config: { dimension: 'score', order: 'desc' }
-                }
-            }
-        ],
-        xAxis: {
-            type: 'category',
-            axisLabel: { interval: 0, rotate: 30 }
-        },
-        yAxis: {},
-        series: {
-            type: 'bar',
-            encode: { x: 'name', y: 'score' },
-            datasetIndex: 1
-        }
-    };
-
-    option && sortedBarchartStores.setOption(option);
-    /* old test code using the Chart.js framework instead of echarts
-   var ctx = document.getElementById('stores-sold').getContext('2d');
-   new Chart(ctx, {
-       type: 'bar',
-       data: {
-           labels: ['Store 1', 'Store 2', 'Store 3', 'Store 4', 'Store 5'],
-           datasets: [{
-               label: 'Number of Pizzas Sold',
-               data: [12, 19, 3, 5, 2],
-               backgroundColor: [
-                   'rgba(54, 162, 235, 0.2)',
-               ],
-               borderColor: [
-                   'rgba(54, 162, 235, 1)',
-               ],
-               borderWidth: 1
-           }]
-       },
-       options: {
-           responsive: true,
-           maintainAspectRatio: true,
-           aspectRatio: 2.5,
-           scales: {
-               yAxes: [{
-                   ticks: {
-                       beginAtZero: true
-                   }
-               }]
-           }
-       }
-   });*/
-}
-
-function testfunc() {
     var sortedBarchartStores = echarts.init(document.getElementById('stores-sold'));
 
     // Original data
@@ -201,4 +63,35 @@ function testfunc() {
     };
     option && sortedBarchartStores.setOption(option);
     sortedBarchartStores.resize({width: 1000, height: 500})
+}
+// tes Function used to test out changes on graphs
+function testfunc(data) {
+    var keys = Object.keys(data[0]);
+    var labelField = keys[0];
+    var valueField = keys[1];
+
+    const overallSold = echarts.init(document.getElementById('line-chart-sales'));
+
+    const storeData = data.map(item => item[labelField]);
+    const categories = data.map(item => item[valueField]);
+
+    var option = {
+        xAxis: {
+            type: 'category',
+            data: categories
+        },
+        yAxis: {
+            type: 'value'
+        },
+        series: [
+            {
+                data: storeData,
+                type: 'line'
+            },
+        ]
+    };
+
+    overallSold.setOption(option);
+    option && overallSold.setOption(option);
+    overallSold.resize({width: 1000, height: 500})
 }

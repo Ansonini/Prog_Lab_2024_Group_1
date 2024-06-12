2f3f9305ba8139921ef0d04995cdd30bb7709a1a
Sargis <lucian-zimmermann@protonmail.com>
2024-06-12 13:07:29 +0200

    Being able to visualize the graphs with the data from the DB using ajax and json.

--- app/Frontend/javascript/over-view.js
+++ app/Frontend/javascript/over-view.js
@@ -1,66 +1,208 @@
-function lineChartSales() {
+$(document).ready(function () {
+    function fetchData() {
+        var view = $('#sold-time').val();
+        var mode = $('#data-display').val();
+        var year = $('#yearDropdown').val();
+        var month = $('#monthDropdown').val();
+        var week = $('#weekDropdown').val();
 
-    const overallSold = echarts.init(document.getElementById('line-chart-sales'));
 
-    var storeData = [150, 230, 224, 218, 135, 147, 260];
-    var categories = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+        var ready = false;
+        switch (view) {
+            case "year":
+                $('#yearDropdown').show();
+                $('#monthDropdown, #weekDropdown').hide();
+                if (mode && year) { ready = true; }
+                break;
+            case "month":
+                $('#yearDropdown, #monthDropdown').show();
+                $('#weekDropdown').hide();
+                if (mode && year && month) { ready = true; }
+                break;
+            case "week":
+                $('#yearDropdown, #weekDropdown').show();
+                $('#monthDropdown').hide();
+                if (mode && year && week) { ready = true; }
+                break;
+        }
+
+        if (ready) {
+            $('#loading-line-chart').show();
+            $.ajax({
+                url: 'ajax/getSalesPerTimeframe.php',
+                type: 'POST',
+                data: {
+                    view: view,
+                    mode: mode,
+                    year: year,
+                    month: month,
+                    week: week
+                },
+                success: function (response) {
+                    if (response.success) {
+                        lineChartSales(response.data, "line-chart-sales");
+                        storesBarChart(response.data, 'stores-sold');
+                    } else {
+                        $('#chart-container').html('<p>' + response.message + '</p>');
+                    }
+                },
+                error: function (xhr, status, error) {
+                    console.log('AJAX Error:', status, error);
+                },
+                complete: function () {
+                    $('#loading-line-chart').hide();
+                }
+            });
+
+            $('#loading-stores-sold').show();
+            $.ajax({
+                url: 'ajax/getSalesPerStore.php',
+                type: 'POST',
+                data: {
+                    view: view,
+                    mode: mode,
+                    year: year,
+                    month: month,
+                    week: week
+                },
+                success: function (response) {
+                    if (response.success) {
+                        lineChartSales(response.data, "line-chart-sales");
+                        storesBarChart(response.data, 'stores-sold');
+                    } else {
+                        $('#chart-container').html('<p>' + response.message + '</p>');
+                    }
+                },
+                error: function (xhr, status, error) {
+                    console.log('AJAX Error:', status, error);
+                },
+                complete: function () {
+                    $('#loading-stores-sold').hide();
+                }
+            });
+        }
+    }
+
+    $('#updateButton').click(fetchData);
+
+    $('#sold-time').change(function() {
+        var view = $(this).val();
+        switch (view) {
+            case "year":
+                $('#yearDropdown').show();
+                $('#monthDropdown, #weekDropdown').hide();
+                break;
+            case "month":
+                $('#yearDropdown, #monthDropdown').show();
+                $('#weekDropdown').hide();
+                break;
+            case "week":
+                $('#yearDropdown, #weekDropdown').show();
+                $('#monthDropdown').hide();
+                break;
+            default:
+                $('#yearDropdown, #monthDropdown, #weekDropdown').hide();
+                break;
+        }
+    }).trigger('change');
+});
+
+function lineChartSales(data, chartID) {
+
+    var keys = Object.keys(data[0]);
+    var storeData = keys[0];
+    var timeValue = keys[1];
+
+    const overAllSold = echarts.init(document.getElementById('line-chart-sales'));
+
+    var existingChart = echarts.getInstanceByDom(overAllSold);
+    if (existingChart) {
+        echarts.dispose(existingChart);
+    }
 
     var option = {
+        title: {
+            text: 'Sales Data'
+        },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow'
+            }
+        },
         xAxis: {
             type: 'category',
-            data: categories
+            data: timeValue
         },
         yAxis: {
             type: 'value'
         },
-        series: [
-            {
-                data: storeData,
-                type: 'line'
-            },
-        ]
+        series: [{
+            name: 'Sales',
+            type: 'bar',
+            data: storeData,
+            itemStyle: {
+                color: 'rgba(75, 192, 192, 0.8)'
+            }
+
+        }]
     };
 
-    overallSold.setOption(option);
-    option && overallSold.setOption(option);
-    overallSold.resize({width: 1000, height: 500})
+    overAllSold.setOption(option);
+    option && overAllSold.setOption(option);
+    overAllSold.resize({width: 1000, height: 500})
 
 }
 function storesBarChart(){
-    var sortedBarchartStores = echarts.init(document.getElementById('stores-sold'));
+    var keys = Object.keys(data[0]);
+    var storeData = keys[0];
+    var storeID = keys[1];
 
-    // Original data
-    var data = [120, 200, 150, 80, 70, 110, 130];
-    var categories = ['Store-ID1', 'Store-ID2', 'Store-ID3', 'Store-ID4', 'Store-ID5', 'Store-ID6', 'Store-ID7'];
+    const sortedBarchartStores = echarts.init(document.getElementById('stores-sold'));
 
-    // Combine data and categories into an array of objects
-    var dataWithCategories = data.map((value, index) => ({
-        value: value,
-        category: categories[index]
+    var existingChart = echarts.getInstanceByDom(sortedBarchartStores);
+    if (existingChart) {
+        echarts.dispose(existingChart);
+    }
+
+    var storeDataWithStoreID = data.map(item => ({
+        value: item[storeData],
+        category: item[storeID]
     }));
 
-    // Sort the array of objects by value in descending order
-    dataWithCategories.sort((a, b) => a.value - b.value);
+    storeDataWithStoreID.sort((a, b) => a.value - b.value);
 
     // Extract the sorted data and categories
-    var sortedData = dataWithCategories.map(item => item.value);
-    var sortedCategories = dataWithCategories.map(item => item.category);
+    var sortedStoreData = storeDataWithStoreID.map(item => item.value);
+    var sortedStoreID = storeDataWithStoreID.map(item => item.category);
 
     var option = {
-        yAxis: {
-            type: 'category',
-            data: sortedCategories
+        title: {
+            text: 'Sales Data'
+        },
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+                type: 'shadow'
+            }
         },
         xAxis: {
+            type: 'category',
+            data: sortedStoreID
+        },
+        yAxis: {
             type: 'value'
         },
-        series: [
-            {
-                data: sortedData,
-                type: 'bar'
-            },
-        ]
+        series: [{
+            name: 'Sales',
+            type: 'bar',
+            data: sortedStoreData,
+            itemStyle: {
+                color: 'rgba(75, 192, 192, 0.8)'
+            }
+        }]
     };
+
     option && sortedBarchartStores.setOption(option);
     sortedBarchartStores.resize({width: 1000, height: 500})
 }

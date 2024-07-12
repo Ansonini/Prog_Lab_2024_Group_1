2f82cb6be1e6896562d9bd2c6363e8da2e50a634
Jean-Gabriel Hanania <83063812+jghanania@users.noreply.github.com>
2024-07-12 15:47:05 +0200

    better grouping of customer coordinates per store + number of visites

--- app/ajax/getCustomerCoordinatesPerStore.php
+++ app/ajax/getCustomerCoordinatesPerStore.php
@@ -8,10 +8,13 @@ include '/var/www/html/ajax/includes/connectDB.php';
 
 
 
-$sql = "SELECT c.customerID, c.latitude
-FROM customers c
-ORDER BY c.customerID";
+$sql = "SELECT storeID, c.customerID, latitude, longitude, visites
+        FROM (SELECT storeID, o.customerID, COUNT(o.customerID) as visites
+            FROM orders o
+            GROUP BY storeID, o.customerID) as sub
+        JOIN customers c on sub.customerID = c.customerID";
 
+$multipleDataset = true;
 
 //make query and return result
 include '/var/www/html/ajax/includes/makeQuery.php';

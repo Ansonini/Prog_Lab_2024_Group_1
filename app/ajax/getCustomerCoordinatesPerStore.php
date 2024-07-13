ef6d01435c098f156a685638c954f5abcb8723b0
Jean-Gabriel Hanania <83063812+jghanania@users.noreply.github.com>
2024-07-13 12:48:08 +0200

    simplified function + added store filter

--- app/ajax/getCustomerCoordinatesPerStore.php
+++ app/ajax/getCustomerCoordinatesPerStore.php
@@ -5,16 +5,22 @@ header('Content-Type: application/json');
 
 // include the DB connect file. ../ because its outside of this folder
 include '/var/www/html/ajax/includes/connectDB.php';
+include '/var/www/html/ajax/includes/checkInput.php';
+
+if ($storeSelection == true && $storeSelection!= 'all') {
+    $storeFilter = "WHERE o.storeID = \"$storeSelection\"";
+} else {
+    $storeFilter = '';
+}
+
+$sql = "SELECT c.customerID, latitude, longitude, visites
+        FROM (SELECT o.customerID, COUNT(o.customerID) as visites
+            FROM orders o 
+            $storeFilter
+            GROUP BY o.customerID) as sub
+                JOIN customers c on sub.customerID = c.customerID;";
 
 
-
-$sql = "SELECT storeID, c.customerID, latitude, longitude, visites
-        FROM (SELECT storeID, o.customerID, COUNT(o.customerID) as visites
-            FROM orders o
-            GROUP BY storeID, o.customerID) as sub
-        JOIN customers c on sub.customerID = c.customerID";
-
-$multipleDataset = true;
 
 //make query and return result
 include '/var/www/html/ajax/includes/makeQuery.php';

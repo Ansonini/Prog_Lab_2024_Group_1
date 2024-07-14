a9c61dfd216b90ed810338bb0b90419eb591ea1a
Jean-Gabriel Hanania <83063812+jghanania@users.noreply.github.com>
2024-07-14 13:26:14 +0200

    made index point to main dashboard + added option to hide legend when only one dataset is present +  some restructuring

--- app/index.php
+++ app/index.php
@@ -1,12 +1,6 @@
 <?php
 // File to set up connection to the Database
 include '/var/www/html/ajax/includes/connectDB.php';
+include 'dashboard.html';
 
-
-// Open .html files that you want to display on homepage http://localhost:8080/ 
-// include 'Frontend/managment_graph.html';
-// include 'Frontend/storeInfo.html';
-
-include 'ajaxExample.html';
-?>
 

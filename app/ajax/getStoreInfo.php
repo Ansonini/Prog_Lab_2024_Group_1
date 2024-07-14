a316a2677884b43790a2f295494b95a58b2f6f23
Jean-Gabriel Hanania <83063812+jghanania@users.noreply.github.com>
2024-07-14 20:31:36 +0200

    can now handle missing storeSelection

--- app/ajax/getStoreInfo.php
+++ app/ajax/getStoreInfo.php
@@ -6,9 +6,16 @@ header('Content-Type: application/json');
 // include the DB connect file. ../ because its outside of this folder
 include '/var/www/html/ajax/includes/connectDB.php';
 
-$storeSelection = $_POST['storeSelection'];
+if (isset($_POST['storeSelection'])) {
+    $storeSelection = $_POST['storeSelection'];
+    $storeFilter = " WHERE o.storeID = \"$storeSelection\"";
+} else {
+    
+    $storeFilter = '';
+}
 
-$sql = "SELECT * from stores where storeID = \"$storeSelection\"";
+
+$sql = "SELECT * from stores $storeFilter ";
 
 
 //make query and return result
